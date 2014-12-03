0ad9aa4a69c4120c42a0c8e8c41ed52dc6293541
Dominik Pfaffenbauer <dominik@lineofcode.at>
2014-12-03 15:40:21 +0100

    added Grid Area

---
+++ views/areas/gallery/view.php
@@ -0,0 +1,43 @@
+
+<? 
+
+    $childs = array();
+    
+    foreach($this->multihref("images")->getElements() as $element)
+    {
+        if($element instanceof Asset_Image)
+        {
+            $childs[] = $element;
+        }
+        else if($element instanceof Asset_Folder)
+        {
+            foreach($element->getChilds() as $child)
+            {
+                if($child instanceof Asset_Image)
+                    $childs[] = $child;
+            }
+        }
+    } 
+    
+    $this->rel = "gallery-" . uniqid();
+    
+    $type = $this->select("type")->getData();
+    $type = $type ? intval($type) : 4;
+
+    $rows = array_chunk($childs, 12/$type);
+?>
+
+
+<? foreach($rows as $childs) { ?>
+    <div class="row">
+    <? foreach ($childs as $asset) { ?>
+        <div class="col-xs-12 col-sm-<?=$type?> col-gallery">
+            <div class="image">
+                <a href="<?=$asset->getThumbnail("lightboxImage")?>" class="fancybox" rel="<?=$this->rel?>" title="<?=$asset->getProperty("lightbox_text")?>">
+                    <img src="<?=$asset->getThumbnail("galleryThumbnail-".$type)?>" class="img-responsive" />
+                </a>
+            </div>
+        </div>
+    <? } ?>
+    </div>
+<? } ?>
