587938dc9a15840e02bb846ffa2f082a5c760d44
Dominik Pfaffenbauer <dominik@lineofcode.at>
2015-01-29 09:14:00 +0100

    remove some obsolete code from columns brick

--- views/scripts/bootstrap/columns/column.php
+++ views/scripts/bootstrap/columns/column.php
@@ -6,27 +6,8 @@
 <? foreach($this->columns as $column) {?>
     <?
         $name = "cs" . $column . "_" . $i;
-        $type = $this->select("container-type-".$name)->getData();
-        $containerStore = array(
-
-        );
-        
-        $fillHeight = $this->checkbox("fill-height-".$name)->isChecked();
-        
     ?>
-    <div class="col-md-<?=$column?> col-sm-<?=$column?> col-xs-12 <?=$type?> <?=$fillHeight ? "to-fill-height" : "" ?>" <?= $fillHeight ? 'data-container=".container"' : ""?>>
-    <?
-        if($this->editmode) {
-            if($this->select("container-type-".$name)->isEmpty()){
-                $this->select("container-type-".$name)->setDataFromResource("container-default");
-            }
-        
-            echo $this->select("container-type-".$name, array("reload" => true, "store" => $containerStore));
-            
-            echo $this->checkbox("fill-height-".$name, array("reload" => true, "boxLabel" => "Fill Height", "width" => 100));
-        }
-    ?>
-    
+    <div class="col-md-<?=$column?> col-sm-<?=$column?> col-xs-12 <?=$type?>">
         <?=$this->template("helper/area.php", array("name" => $name))?>
     </div>
     
